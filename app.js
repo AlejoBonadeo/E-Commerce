4894b769ee7ee9498f78b10851a34ad50af21918
javomenendez <84793170+javomenendez@users.noreply.github.com>
2021-08-31 10:59:46 -0300

    Se creo stylesheet - logo.png

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.listen(3030, ()=>console.log("Servidor iniciado - escuchando en puerto 3030"));
+app.use('/static', express.static(__dirname + '/public'));
+
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/index.html')));
+
+app.listen(8080, ()=>console.log("Servidor iniciado - escuchando en puerto 3030"));
