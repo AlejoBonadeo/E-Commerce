0b63f427644980449c9929c86352d52cfd893cfa
AlejoBonadeo <alejobonadeo@hotmail.com>
2021-09-07 18:04:58 -0300

    Se organizaron las carpetas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname,'/src/public')));
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/views/index.html'))
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 
 app.listen(8080, ()=>console.log("Servidor iniciado - escuchando en puerto 8080"));
