15f50757d5aab8fcae290851fef401e3e7a8f86b
AlejoBonadeo <alejobonadeo@hotmail.com>
2021-09-08 18:16:19 -0300

    Se creó el carrito, con su css y rutas en app.js

--- src/app.js
+++ src/app.js
@@ -8,4 +8,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/carrito.html'))
+});
+
 app.listen(8080, ()=>console.log("Servidor iniciado - escuchando en puerto 8080"));
