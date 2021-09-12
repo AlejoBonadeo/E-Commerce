c273db29709fc79dc77d26970f24fd2ca2af069b
AlejoBonadeo <alejobonadeo@hotmail.com>
2021-09-12 15:40:55 -0300

    Se separó el elemento HTML de nav-bar a un archivo nav-bar.js

--- src/public/javascript/categories.js
+++
@@ -1,6 +0,0 @@
-const categoriesContainer = document.querySelector(".categorias-container")
-const categoriesButton = document.querySelector(".categorias-btn")
-
-categoriesButton.addEventListener("click", () => {
-    categoriesContainer.classList.toggle("mostrar-categorias")
-})
