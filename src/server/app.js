2625fdd42901426cdf1e8a8bef82d891fd3191ad
AlejoBonadeo <alejobonadeo@hotmail.com>
2021-09-13 21:38:04 -0300

    Terminamos de arreglar la estructura de directorios

--- src/server/app.js
+++ src/server/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname,'/public')));
+const public = path.join(__dirname,"../web/public")
+
+app.use(express.static(public));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/index.html'))
+    res.sendFile(path.join(public, '/views/index.html'))
 });
 
 app.get('/carrito', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/carrito.html'))
+    res.sendFile(path.join(public, '/views/carrito.html'))
 });
 
 app.listen(8080, ()=>console.log("Servidor iniciado - escuchando en puerto 8080"));
