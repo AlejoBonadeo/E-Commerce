ab31bdb72d2335aa160e5cc5f309960e455e0083
AlejoBonadeo <alejobonadeo@hotmail.com>
2021-09-29 22:30:42 -0300

    Se reestucturaron los directorios y se agregaron los controllers

--- src/server/app.js
+++ src/server/app.js
@@ -1,36 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { index, carrito, producto, crearproducto, login, register } = require("./controllers/webController");
 
 const public = path.join(__dirname, "../web/public");
 
 app.use(express.static(public));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(public, "/views/index.html"));
-});
+app.get("/", index);
 
-app.get("/carrito", (req, res) => {
-  res.sendFile(path.join(public, "/views/carrito.html"));
-});
+app.get("/carrito", carrito);
 
-app.get("/producto", (req, res) => {
-  res.sendFile(path.join(public, "/views/producto.html"));
-});
+app.get("/producto/:id", producto);
 
-app.get("/register", (req, res) => {
-  res.sendFile(path.join(public, "/views/register.html"));
-});
+app.get("/register", register);
 
-app.get("/login", (req, res) => {
-  res.sendFile(path.join(public, "/views/login.html"));
-});
+app.get("/login", login);
 
-app.get("/crearproducto", (req, res) => {
-  res.sendFile(path.join(public, "/views/crearproducto.html"));
-});
+app.get("/crearproducto", crearproducto);
 
 app.listen(8080, () =>
   console.log("Servidor iniciado - escuchando en puerto 8080")
 );
-
