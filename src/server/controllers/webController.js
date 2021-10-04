2af4ca97dd9f1f1390228348a12551a5e7d6ece4
AlejoBonadeo <alejobonadeo@hotmail.com>
2021-10-04 01:01:32 -0300

    Se des-hardocodearon los libros(ponele)

--- src/server/controllers/webController.js
+++ src/server/controllers/webController.js
@@ -1,9 +1,10 @@
 const path = require("path");
+const data = require("../data");
 const views = path.join(__dirname, "../../web/views");
 
 module.exports = {
     index: (req, res) => {
-        res.render(path.join(views, "/index"));
+        res.render(path.join(views, "/index"), {libros: data});
     },
     carrito: (req, res) => {
         res.render(path.join(views, "/carrito"));
---
+++ src/server/data.js
@@ -0,0 +1,74 @@
+const path = "/img/dummy-books/book-"
+
+module.exports = [
+    {
+        id: 0,
+        name: "The C Programming Language 2nd Edition",
+        author: "Brian W. Kernighan, Dennis M. Ritchie",
+        category: "Tecnologia",
+        img: path + 1 + ".jfif"
+    },
+    {
+        id: 1,
+        name: "Introduction to Algorithms, 3rd Edition",
+        author: "Thomas H. Cormen",
+        category: "Tecnologia",
+        img: path + 2 + ".jfif"
+    },
+    {
+        id: 2,
+        name: "The C++ Programming Language",
+        author: "Bjarne Stroustrup",
+        category: "Tecnologia",
+        img: path + 3 + ".jfif"
+    },
+    {
+        id: 3,
+        name: "C++ How to Program",
+        author: "Harvey Deitel, Paul Deitel",
+        category: "Tecnologia",
+        img: path + 4 + ".jfif"
+    },
+    {
+        id: 4,
+        name: "Fundamentals of Software Engineering 2nd Edition",
+        author: "Carlo Ghezzi, Dino Mandrioli",
+        category: "Tecnologia",
+        img: path + 5 + ".jfif"
+    },
+    {
+        id: 5,
+        name: "Quimica Basica",
+        author: "Di Risio",
+        category: "Ciencia",
+        img: path + 6 + ".jfif"
+    },
+    {
+        id: 6,
+        name: "Fisica Universitaria Vol. 1",
+        author: "Sears, Zemansky",
+        category: "Ciencia",
+        img: path + 7 + ".jfif"
+    },
+    {
+        id: 7,
+        name: "IB Biology",
+        author: "Andrew Allott, David Mindorff",
+        category: "Ciencia",
+        img: path + 8 + ".jfif"
+    },
+    {
+        id: 8,
+        name: "Fisica para la Ciencia y la tecnologia 6ta Edicion",
+        author: "Tripler, Mosca",
+        category: "Ciencia",
+        img: path + 9 + ".jfif"
+    },
+    {
+        id: 9,
+        name: "Introduccion al Analisis Matematico",
+        author: "Hebe T. Rabuffetti",
+        category: "Ciencia",
+        img: path + 10 + ".jfif"
+    },
+]
