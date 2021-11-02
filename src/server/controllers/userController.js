25ef47d0331c92eac7aa8f47703e4955fb188900
AlejoBonadeo <alejobonadeo@hotmail.com>
2021-11-02 17:40:06 -0300

    se agrego la funcionalidad de crear usuario

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -1,3 +1,9 @@
+const { hashSync, genSaltSync } = require("bcryptjs");
+const fs = require('fs');
+const path = require("path");
+
+const usersFilePath = path.resolve(__dirname, "../data/Users.json");
+
 const userController = {
   register: (req, res) => {
     res.render("./user/register");
@@ -5,6 +11,38 @@ const userController = {
   login: (req, res) => {
     res.render("./user/login");
   },
+  newAccount: (req, res) => {
+    //TODO agregar multer
+    let users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"))
+    const { body } = req
+
+    if( body.passUsuario !== body.repeatpassUsuario ) {
+      return res.render('./user/register', { error: 'Ambas contraseñas deben ser iguales'})
+    }
+    for(user of users) {
+      if( user.emailUsuario === body.emailUsuario ) {
+        return res.render('./user/register', { error: 'El email ya esta registrado'})
+      }
+    }
+
+    const salt = genSaltSync()
+    body.passUsuario = hashSync( body.passUsuario, salt )
+
+    delete body.aceptaTerminos
+    delete body.repeatpassUsuario
+
+    users = [...users, {
+      idUsuario: new Date().getTime(),
+      ...body
+    }]
+    
+    fs.writeFileSync(
+      usersFilePath,
+      JSON.stringify( users, null, 3 )
+    )
+
+    res.redirect('/')
+  }
 };
 
 module.exports = userController;
--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const validateForm = require("../middlewares/validateForm");
 
 /* GET home page. */
 router.get("/register", userController.register);
 
 router.get("/login", userController.login);
+
+router.post(
+    '/register',
+    [
+        check('nombreDeUsuario', 'Por favor ingrese un nombre válido').not().isEmpty(),
+        check('emailUsuario', 'Por favor ingrese un email válido').isEmail(),
+        check('passUsuario', 'La contraseña debe tener al menos 7 caracteres').isLength({min: 6}),
+        validateForm()
+    ] 
+    ,userController.newAccount
+)
 
 module.exports = router;
