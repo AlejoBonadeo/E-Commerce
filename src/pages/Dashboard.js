90afb5ec3b608b2b57d2d7625477c74ac7d3e202
fenglebienne <felipe.englebienne@gmail.com>
2022-01-22 03:05:23 -0300

    a ver que onda

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Grid} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+import Navbar from '../components/Navbar'
+import 'fontsource-roboto';
+import '../assets/css/Dashboard.css'
+const useStyles = makeStyles (() =>({
+    root:{
+        flexGrow: 1
+    }
+}))
+
+function Dashboard (props){
+    const classes = useStyles();
+    return(
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Navbar/>
+                </Grid>
+
+            </Grid>
+        </div>
+    );
+}
+
+export default Dashboard;
